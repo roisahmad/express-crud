24bd27a40dd38046ad35d28995d7f7b98a9eb986
Ahmad Rois <ahmadlutfirois@gmail.com>
2024-05-06 07:48:53 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.send("Selamat datang di API akuh");
+});
+
+const productController = require("./product/product.controller");
+
+app.use("/products", productController);
+
+app.listen(PORT, () => {
+  console.log("Express API running in port: " + PORT);
+});
